08cfe5a3cd88ae80027e13804a50a131ef5d4770
John Weaver <johnweaver@zohomail.com>
2024-01-15 15:55:12 -0500

    added filtering to get cities get request

--- Resources.API/Services/ICityInfoRepository.cs
+++ Resources.API/Services/ICityInfoRepository.cs
@@ -6,6 +6,7 @@ namespace Resources.API.Services
     {
         Task<bool> CityExists(int cityId);
         Task<IEnumerable<City>> GetCitiesAsync();
+        Task<IEnumerable<City>> GetCitiesAsync(string? name, string? searchQuery);
         Task<City>? GetCityAsync(int cityId, bool includePointsOfInterest);
         Task<PointOfInterest>? GetPointOfInterestForCityAsync(int cityId,
             int pointOfInterestId);
